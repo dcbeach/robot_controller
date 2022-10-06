a5b3e52677012238ed38c4b542ce1695fb229a00
dcbeach <douglas.c.beach@gmail.com>
2022-10-06 19:53:24 -0400

    Trying new IMU code

--- robot_controller/mpu6050_node.py
+++ robot_controller/mpu6050_node.py
@@ -4,93 +4,40 @@ from rclpy.node import Node
 import smbus			#import SMBus module of I2C
 from time import sleep   
 from geometry_msgs.msg import Twist
-
-#some MPU6050 Registers and their Address
-PWR_MGMT_1   = 0x6B
-SMPLRT_DIV   = 0x19
-CONFIG       = 0x1A
-GYRO_CONFIG  = 0x1B
-INT_ENABLE   = 0x38
-ACCEL_XOUT_H = 0x3B
-ACCEL_YOUT_H = 0x3D
-ACCEL_ZOUT_H = 0x3F
-GYRO_XOUT_H  = 0x43
-GYRO_YOUT_H  = 0x45
-GYRO_ZOUT_H  = 0x47
-
-bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
-Device_Address = 0x68   # MPU6050 device address
+from mpu6050 import mpu6050
 
 class MyNode(Node):
 
     def __init__(self):
         super().__init__("imu_node")
+        self.mpu = mpu6050(0x68)
         self.cmd_vel_pub_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
         self.timer_ = self.create_timer(0.5, self.send_imu_command)
         self.get_logger().info("IMU Node has started.")
     
     def send_imu_command(self):
+        accel_data = self.mpu.get_accel_data()
         #Read Accelerometer raw value
-        acc_x = read_raw_data(ACCEL_XOUT_H)
-        acc_y = read_raw_data(ACCEL_YOUT_H)
-        acc_z = read_raw_data(ACCEL_ZOUT_H)
-        
+        acc_x = accel_data['x']
+        acc_y = accel_data['y']
+        acc_z = accel_data['z']
+        gyro_data = self.mpu.get_gyro_data()
         #Read Gyroscope raw value
-        gyro_x = read_raw_data(GYRO_XOUT_H)
-        gyro_y = read_raw_data(GYRO_YOUT_H)
-        gyro_z = read_raw_data(GYRO_ZOUT_H)
-        
-        #Full scale range +/- 250 degree/C as per sensitivity scale factor
-        Ax = acc_x/16384.0
-        Ay = acc_y/16384.0
-        Az = acc_z/16384.0
-        
-        Gx = gyro_x/131.0
-        Gy = gyro_y/131.0
-        Gz = gyro_z/131.0
+        gyro_x = gyro_data['x']
+        gyro_y = gyro_data['y']
+        gyro_z = gyro_data['z']
 
         msg = Twist()
-        msg.linear.x = Ax
-        msg.linear.y = Ay
-        msg.linear.z = Az
-        msg.angular.x = Gx
-        msg.angular.y = Gy
-        msg.angular.z = Gz
+        msg.linear.x = acc_x
+        msg.linear.y = acc_y
+        msg.linear.z = acc_z
+        msg.angular.x = gyro_x
+        msg.angular.y = gyro_y
+        msg.angular.z = gyro_z
         self.cmd_vel_pub_.publish(msg)
-        
-def MPU_Init():
-	#write to sample rate register
-	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
-	
-	#Write to power management register
-	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
-	
-	#Write to Configuration register
-	bus.write_byte_data(Device_Address, CONFIG, 0)
-	
-	#Write to Gyro configuration register
-	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-	
-	#Write to interrupt enable register
-	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
-
-def read_raw_data(addr):
-	#Accelero and Gyro value are 16-bit
-        high = bus.read_byte_data(Device_Address, addr)
-        low = bus.read_byte_data(Device_Address, addr+1)
-    
-        #concatenate higher and lower value
-        value = ((high << 8) | low)
-        
-        #to get signed value from mpu6050
-        if(value > 32768):
-                value = value - 65536
-        return value
-
 
 def main(args=None):
     rclpy.init(args=args)
-    MPU_Init()
 
     #CODE GOES HERE
     node = MyNode()
